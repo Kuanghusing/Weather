4f578c75980a353572d44995145ee1b16b9e501d
kuahusg <foreverhgx@gmail.com>
2016-06-16 23:26:21 +0800

    UI微调

--- app/src/main/java/com/kuahusg/weather/activities/Setting.java
+++ app/src/main/java/com/kuahusg/weather/activities/Setting.java
@@ -1,20 +1,43 @@
 package com.kuahusg.weather.activities;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.kuahusg.weather.Fragment.SettingFrag;
+import com.kuahusg.weather.R;
 
 /**
  * Created by kuahusg on 16-6-10.
  * com.kuahusg.weather.activities
  */
-public class Setting extends PreferenceActivity {
+public class Setting extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        addPreferencesFromResource(R.xml.preference);
-//        setContentView(R.layout.setting);
+        setContentView(R.layout.setting);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
 
 /*
         ViewGroup viewRoot = (ViewGroup) findViewById(android.R.id.content);
@@ -25,7 +48,7 @@ public class Setting extends PreferenceActivity {
         toolbarLayout.addView(content);
         viewRoot.addView(toolbarLayout);*/
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingFrag()).commit();
+        getFragmentManager().beginTransaction().replace(R.id.setting_part, new SettingFrag()).commit();
 
 
     }
