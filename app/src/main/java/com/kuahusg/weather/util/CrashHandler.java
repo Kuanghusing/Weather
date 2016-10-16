1709662157ff621388e04f66ab60fe3cf1042486
kuahusg <foreverhgx@gmail.com>
2016-10-16 17:34:24 +0800

    feat CrashHandler
    - before handle uncaugthException, show a toast and a notification to
    let the user to send the crash log

--- app/src/main/java/com/kuahusg/weather/util/CrashHandler.java
+++ app/src/main/java/com/kuahusg/weather/util/CrashHandler.java
@@ -1,9 +1,20 @@
 package com.kuahusg.weather.util;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Looper;
+import android.os.Message;
 import android.os.Process;
 import android.os.StrictMode;
+import android.support.annotation.RequiresApi;
 import android.widget.Toast;
 
 import com.kuahusg.weather.App;
@@ -25,11 +36,12 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         if (throwable != null && exceptionHandler != null) {
             handleException(throwable);
             try {
-                Thread.sleep(4000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
 
             }
             exceptionHandler.uncaughtException(thread, throwable);
+            //这个好像要这个方法处理，不然会调用多次handleException()??
         } else {
 
             Process.killProcess(Process.myPid());
@@ -50,23 +62,34 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
 
     private void handleException(final Throwable e) {
         final StackTraceElement[] elements = e.getStackTrace();
-        final String message = e.getMessage();
+        final String message = "Message:\n" + e.getMessage() + "\n";
         final String fileName = "crash-" + System.currentTimeMillis() + ".log";
 
 
+        StringBuilder sb = new StringBuilder();
+        for (StackTraceElement element : elements) {
+            sb.append(element.toString()).append("\n");
+        }
+        final String stackMessage = "\nTraceMessage: " + sb.toString();
+
+
+        final String toastMessage = App.getContext().getString(R.string.crash_message);
+
+
+        setNotification(message + stackMessage + getDeviceInfo());
         new Thread(new Runnable() {
             @Override
             public void run() {
                 Looper.prepare();
-                Toast.makeText(App.getContext(), App.getContext().getString(R.string.crash_message)
-                        + App.getContext().getExternalFilesDir(null) + "/" + fileName, Toast.LENGTH_LONG).show();
+                Toast.makeText(App.getContext(), toastMessage, Toast.LENGTH_LONG).show();
                 try {
                     final FileOutputStream fos = new FileOutputStream(new File(App.getContext()
                             .getExternalFilesDir(null), fileName), true);
-                    fos.write(("message:" + message + "\n").getBytes());
-                    for (StackTraceElement element : elements) {
-                        fos.write((element.toString() + "\n").getBytes());
-                    }
+                    fos.write((message).getBytes());
+
+                    fos.write((stackMessage).getBytes());
+                    fos.write(getDeviceInfo().getBytes());
+
 
                     fos.flush();
                     fos.close();
@@ -86,6 +109,52 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
 
     }
 
+
+    private void setNotification(String message) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"foreverhgx@gmail.com"});
+            intent.putExtra(Intent.EXTRA_TEXT, message);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Crash Report");
+            intent.setType("text/plain");
+
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(App.getContext(), 0,
+                    intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            Notification notification = null;
+            Notification.Builder builder;
+            builder = new Notification.Builder(App.getContext())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(App.getContext().getString(R.string.solve_crash_message))
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                builder.setLargeIcon(Icon.createWithResource(App.getContext(), R.mipmap.ic_launcher));
+            }
+            notification = builder.build();
+
+            NotificationManager notificationManager = (NotificationManager) App.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0, notification);
+        }
+
+    }
+
+    private String getDeviceInfo() {
+        PackageManager packageManager = App.getContext().getPackageManager();
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            stringBuilder.append("\nDevices Info:")
+                    .append("\nSDK_INT: ").append(Build.VERSION.SDK_INT)
+                    .append("\nDEVICE: ").append(Build.DEVICE)
+                    .append("\nPackageVersionName: ").append(packageManager.getPackageInfo(App.getContext().getPackageName(), 0).versionName)
+                    .append("\nPackageVersionCode: ").append(packageManager.getPackageInfo(App.getContext().getPackageName(), 0).versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
 
     private static class InstanceHolder {
         static final CrashHandler INSTANCE = new CrashHandler();
